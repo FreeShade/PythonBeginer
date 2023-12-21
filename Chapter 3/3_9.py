a01cda5060f68c0edafad1465222aead6ea33aea
FreeShade <84828176+FreeShade@users.noreply.github.com>
2023-12-21 12:21:10 +0200

    Create 3_9.py

---
+++ Chapter 3/3_9.py
@@ -0,0 +1,44 @@
+guest_list = ["Adam","Selina","Eric", "Debora"]
+
+cant_come = guest_list.pop(-1)
+can_come = guest_list.append("Susan")
+
+guest_list.insert(0, "Philip")
+
+guest_list.insert(2, "Hector")
+
+guest_list.append("Maria")
+
+invitation = f"{guest_list} welcome to the party."
+
+cat_come_invitation = f"{cant_come.title()} cant come today, bitch"
+
+
+
+print(cat_come_invitation)
+
+print("I was buy a bigger table, so I can get more guests")
+
+
+print(invitation)
+
+print("Sorry guys, I can invite only two guests")
+
+print(f"{guest_list.pop()}, soory , you cant come.")
+print(f"{guest_list.pop()}, soory , you cant come.")
+print(f"{guest_list.pop()}, soory , you cant come.")
+print(f"{guest_list.pop()}, soory , you cant come.")
+print(f"{guest_list.pop()}, soory , you cant come.")
+
+
+new_invitation1 = guest_list[0]
+new_invitation2 = guest_list[1]
+print(f"{new_invitation1} and {new_invitation2}, you steal welcome")
+
+print(len(guest_list))
+
+
+del guest_list[0]
+del guest_list[0]
+
+print(guest_list)
