f59bc1d5571b0f54e752df98145025565199ffd3
FreeShade <84828176+FreeShade@users.noreply.github.com>
2023-12-21 12:21:44 +0200

    Create 4_2.py

---
+++ Chapter 4/4_2.py
@@ -0,0 +1,8 @@
+# Animals 
+
+animals = ['cat', 'dog', 'seagul', 'swallow', 'owl'] 
+
+for animal in animals:
+    print(f'A {animal.title()} would make a great pet')
+
+print('Any of these animals would make a great pet!')
