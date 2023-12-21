979c17594afa105a1409fd322179d57f3137747e
FreeShade <84828176+FreeShade@users.noreply.github.com>
2023-12-21 12:22:10 +0200

    Create Dirty_Page.py

---
+++ Dirty Page/Dirty_Page.py
@@ -0,0 +1,35 @@
+# players = ["charles", "martina", " michael", "florence", "eli"]
+# print(players[0:3])
+# print(players[1:4])
+# print(players[:4])
+# print(players[2:])
+# print(players[-3:])
+
+
+# players = ["charles", "martina", "michael", "florence", "eli"]
+# print("Here are the first three players on my team:")
+# for player in players[:3]:
+#    print(player.title())
+
+
+# my_foods = ["pizza", "falafel", "carrot cake"]
+# friend_foods = my_foods[:]
+
+# my_foods.append("cannoli")
+# friend_foods.append("ice ceam")
+
+
+# print("My favorite foods are:")
+# print(my_foods)
+
+# print("\nMy friend`s favorite foods are:")
+# print(friend_foods)
+
+
+# Tuples Кортежі , не любив їх і не люблю.
+
+
+# for i in range(30):
+#    i += 1
+#    print("*" * i)
+# input()
