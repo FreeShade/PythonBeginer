e7114fd2fb930be2ab66b8312e46a250a9994e04
FreeShade <84828176+FreeShade@users.noreply.github.com>
2023-12-22 16:39:45 +0200

    Update 5_1.py

---
+++ Chapter 5/5_1.py
@@ -0,0 +1,34 @@
+car = "subaru"
+print("Is car == 'subaru' ? I predict True")
+print(car == "subaru")
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == "audi")
+
+
+food = "fish"
+print(f"Is {food} = fish ?")
+print(food == "fish")
+
+
+car = "BMW"
+print("Is car == 'subaru' ? I predict True")
+print(car == "subaru")
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == "audi")
+
+
+car = "Mercedes"
+print("Is car == 'subaru' ? I predict True")
+print(car == "subaru")
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == "audi")
+
+car = "audi"
+print("Is car == 'subaru' ? I predict True")
+print(car == "subaru")
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == "audi")
