0e780250325f37e12454c66d91213bb6b629250f
FreeShade <84828176+FreeShade@users.noreply.github.com>
2023-12-22 16:39:52 +0200

    Update Dirty_Page.py

--- Dirty Page/Dirty_Page.py
+++ Dirty Page/Dirty_Page.py
@@ -33,3 +33,80 @@
 #    i += 1
 #    print("*" * i)
 # input()
+
+# --------------------------------------------------------------------------------------------------
+# П'ятий розділ , боротьба з 'if' думаю в цей раз буде простіше.
+# --------------------------------------------------------------------------------------------------
+
+# cars = ["audi", "bmw", "subaru", "toyota"]
+
+# for car in cars:
+#     if car == "bmw":
+#         print(car.upper())
+#     else:
+#         print(car.title())
+
+# car = "bmw"
+# print(car == "bmw")
+
+# car = "audi"
+# print(car == "bmw")
+
+
+# car = "bmw"
+# print(car == "audi")
+
+
+# car = "Audi"
+# print(car.lower() == "audi")
+
+
+# requested_topping = "mushrooms"
+
+# if requested_topping != "anchovies":
+#     print("Hold the anchovies !")
+
+
+# age = 18
+# print(age == 18)
+
+
+# answer = 17
+
+# if answer != 42:
+#     print("That is not the correct answer. please try again!")
+
+
+# age = 19
+# print(age < 21)
+# print(age <= 21)
+# print(age > 21)
+# print(age >= 21)
+
+
+# age_0 = 22
+# age_1 = 18
+# print(age_0 >= 21 and age_1 >= 21)
+
+# age_1 = 22
+
+# print(age_0 >= 21 and age_1 >= 21)
+
+
+# age_0 = 22
+# age_1 = 18
+# print(age_0 >= 21 or age_1 >= 21)
+
+# requested_toppings = ["mushrooms", "onions", "pineapple"]
+# print("mushrooms" in requested_toppings)
+# print("peperoni" in requested_toppings)
+
+
+# banned_users = ["andrew", "carolina", "david"]
+# user = "marie"
+# if user not in banned_users:
+#     print(f"{user.title()}, you can post a response if you wish.")
+
+
+game_active = True
+cant_edit = False
