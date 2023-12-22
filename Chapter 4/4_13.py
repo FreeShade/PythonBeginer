cc335684cc21a1a8c10c73b88d49d8ae2e0aad60
FreeShade <84828176+FreeShade@users.noreply.github.com>
2023-12-22 11:03:35 +0200

    Update 4_13.py

---
+++ Chapter 4/4_13.py
@@ -0,0 +1,12 @@
+dishes = ("dish 1", "dish 2", "dish 3", "dish 4", "dish 5")
+
+for dish in dishes:
+    print(dish.title())
+
+# dishes[0] = "new dish"
+
+
+dishes = ("new dish 1", "new dish 2", "dish 3", "dish 4", "dish 5")
+
+for dish in dishes:
+    print(dish.title())
