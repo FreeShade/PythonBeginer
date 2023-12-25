cebcc8c669b0e216a4eab4c55152a49c39698d86
FreeShade <84828176+FreeShade@users.noreply.github.com>
2023-12-25 15:17:59 +0200

    Update Dirty_Page.py

--- Dirty Page/Dirty_Page.py
+++ Dirty Page/Dirty_Page.py
@@ -108,5 +108,39 @@
 #     print(f"{user.title()}, you can post a response if you wish.")
 
 
-game_active = True
-cant_edit = False
+# game_active = True
+# cant_edit = False
+
+
+# if conditional_test:
+#     do something
+
+# age = 17
+
+# if age >= 18:
+#     print("You are old enough to vote!")
+#     print("Have you registered to vote yet?")
+# else:
+#     print("Sorry, you are too young to vote.")
+#     print("Please register to vote as soon as you turn 18!")
+
+# age = 12
+
+# if age < 4:
+#     print("your admission cost is $0.")
+# elif age < 18:
+#     print("Your admission cost is $25")
+# else:
+#     print("Your admission cost is $40.")
+
+
+age = 12
+
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 25
+else:
+    price = 40
+
+print(f"You admission cost is ${price}")
