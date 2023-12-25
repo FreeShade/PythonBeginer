838e725692b50199fda64bd0cfe56d950d55d725
FreeShade <84828176+FreeShade@users.noreply.github.com>
2023-12-25 15:26:53 +0200

    Update Dirty_Page.py

--- Dirty Page/Dirty_Page.py
+++ Dirty Page/Dirty_Page.py
@@ -134,13 +134,27 @@
 #     print("Your admission cost is $40.")
 
 
+# age = 12
+
+# if age < 4:
+#     price = 0
+# elif age < 18:
+#     price = 25
+# else:
+#     price = 40
+
+# print(f"You admission cost is ${price}")
+
+
 age = 12
 
 if age < 4:
     price = 0
 elif age < 18:
     price = 25
-else:
+elif age < 65:
     price = 40
+else:
+    price = 20
 
-print(f"You admission cost is ${price}")
+print(f"Your admission cost is ${price}.")
