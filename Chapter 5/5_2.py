2317d8108b703519a6d214d93ee6598bac4ec43e
FreeShade <84828176+FreeShade@users.noreply.github.com>
2023-12-25 15:04:37 +0200

    Update 5_2.py

--- Chapter 5/5_2.py
+++ Chapter 5/5_2.py
@@ -6,9 +6,17 @@ cars = ["audi", "bmw", "mercedes", "subaru", "toyota"]
 
 # print(cars == "audi" and cars == "bmw")
 
+user = "something"
 
 for car in cars:
     if car == "bmw" or car == "subaru":
         print(f"{car.title()}, you can drive it.")
+
 print("bmw" in cars)
+
 print("honda" in cars)
+
+print("audi" in cars)
+
+if user not in cars:
+    print(f"{user.title()}, you ae not welcome here.")
