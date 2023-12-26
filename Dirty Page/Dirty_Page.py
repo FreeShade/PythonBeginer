7aea2c3d60f949ab750ceb5db1ae26963b787b23
FreeShade <84828176+FreeShade@users.noreply.github.com>
2023-12-26 13:13:03 +0200

    Update Dirty_Page.py

--- Dirty Page/Dirty_Page.py
+++ Dirty Page/Dirty_Page.py
@@ -146,15 +146,40 @@
 # print(f"You admission cost is ${price}")
 
 
-age = 12
+# age = 12
 
-if age < 4:
-    price = 0
-elif age < 18:
-    price = 25
-elif age < 65:
-    price = 40
-else:
-    price = 20
+# if age < 4:
+#     price = 0
+# elif age < 18:
+#     price = 25
+# elif age < 65:
+#     price = 40
+# else:
+#     price = 20
 
-print(f"Your admission cost is ${price}.")
+# print(f"Your admission cost is ${price}.")
+
+# age = 12
+
+# if age < 4:
+#     price = 0
+# elif age < 18:
+#     price = 25
+# elif age < 65:
+#     price = 40
+# elif age >= 65:
+#     price = 20
+
+# print(f"Your admission cost is ${price}.")
+
+
+requested_toppings = ["mushrooms", "extra cheese"]
+
+if "mushrooms" in requested_toppings:
+    print("Adding mushrooms.")
+if "pepperoni" in requested_toppings:
+    print("Adding pepperoni.")
+if "extra cheese" in requested_toppings:
+    print("Adding extra cheese.")
+
+print("\nFinished making your pizza.")
