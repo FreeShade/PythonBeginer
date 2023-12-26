e0a28fbd0c678e3af2b0263e5fd21e7549f99f82
FreeShade <84828176+FreeShade@users.noreply.github.com>
2023-12-26 13:12:57 +0200

    Create 5_3.py

---
+++ Chapter 5/5_3.py
@@ -0,0 +1,19 @@
+# придумай кольори інопланетян
+
+alien_color = ["green", "yellow", "red", "blue"]
+
+score = 0
+
+if "green" in alien_color:
+    score += 5
+    print(f"You kill green Alien , you got 5 points")
+# Повертає вивід
+
+alien_color = ["green", "yellow", "red", "blue"]
+
+score = 0
+
+if "black" in alien_color:
+    score += 5
+    print(f"You kill green Alien , you got 5 points")
+# Повертає "хибу" , тобто не має жодного виводу.
