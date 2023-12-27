4f5699944e2a5db7b87b9f0ffc182cfc118fb894
FreeShade <84828176+FreeShade@users.noreply.github.com>
2023-12-27 15:06:36 +0200

    Create 5_7.py

---
+++ Chapter 5/5_7.py
@@ -0,0 +1,15 @@
+favorite_fruits = ["apple", "orange", "bananas"]
+
+if "apple" in favorite_fruits:
+    print("You really like apples.")
+if "orange" in favorite_fruits:
+    print("You really like oranges.")
+if "bananas" in favorite_fruits:
+    print("You really like bananas.")
+if "cherry" not in favorite_fruits:
+    print("You dont like cherries.")
+if "peer" not in favorite_fruits:
+    print("You dont like peers.")
+
+
+# Урізноманітнив завданням виводом того що немає. Просто щоб вивело всі блоки.
