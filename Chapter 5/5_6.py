dd811a604cb619370d70c1b235032635cbe91e30
FreeShade <84828176+FreeShade@users.noreply.github.com>
2023-12-27 15:06:33 +0200

    Create 5_6.py

---
+++ Chapter 5/5_6.py
@@ -0,0 +1,20 @@
+# Phase of life
+# Писатиму через інпут , бо замахався в ручну міняти в коді змінну
+
+age = 70
+
+if age <= 2:
+    print("You are a newborn.")
+elif age in range(2, 4):
+    print("You are a babby.")
+elif age in range(2, 4):
+    print("You are a childe.")
+elif age in range(4, 13):
+    print("You are a teenager.")
+elif age in range(20, 65):
+    print("you are an adult.")
+elif age >= 65:
+    print("You are granny.")
+
+
+# Використав ін рендж бо це звучало простіше і веселіше , ну і проканало.
