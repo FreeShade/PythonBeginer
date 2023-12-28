411cee5b220fd908ab405be8ad9f07247e7202b3
FreeShade <84828176+FreeShade@users.noreply.github.com>
2023-12-28 16:09:53 +0200

    Update Dirty_Page.py

--- Dirty Page/Dirty_Page.py
+++ Dirty Page/Dirty_Page.py
@@ -195,12 +195,43 @@
 # print("\nFinished making your pizza!")
 
 
-requested_toppings = ["mushrooms", "green peppers", "extra cheese"]
+# requested_toppings = ["mushrooms", "green peppers", "extra cheese"]
 
+# for requested_topping in requested_toppings:
+#     if requested_topping == "green peppers":
+#         print("Sorry, we are out of green peppers right now.")
+#     else:
+#         print(f"Adding {requested_topping}.")
+
+# print("\nFinished making your pizza!")
+
+
+# requested_toppings = []
+
+# if requested_toppings:
+#     for requested_topping in requested_toppings:
+#         print(f"Adding {requested_topping}.")
+#     print("\nFinished making your pizza!")
+# else:
+#     print("Are you sure you want a plain pizza?")
+
+
+# Step 1  визначаємо список доступних інгредієнтів
+available_toppings = [
+    "mushrooms",
+    "olives",
+    "green peppers",
+    "pepperoni",
+    "pineapple",
+    "extra cheese",
+]
+# Step 2 список замовлених інгредієгтів
+requested_toppings = ["mushrooms", "french fries", "extra cheese"]
+# Step 3 проходимо по спуску, якщо є то додаємо , якщо немає то виводимо else
 for requested_topping in requested_toppings:
-    if requested_topping == "green peppers":
-        print("Sorry, we are out of green peppers right now.")
-    else:
+    if requested_topping in available_toppings:
         print(f"Adding {requested_topping}.")
+    else:
+        print(f"Sorry, we don`t have {requested_topping}.")
 
-print("\nFinished making your pizza!")
+print("\nFinished making your pizza.")
