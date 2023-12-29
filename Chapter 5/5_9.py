90e0a8c9ffc91e506cbe52188e6befb9737864e3
FreeShade <84828176+FreeShade@users.noreply.github.com>
2023-12-29 14:06:24 +0200

    Create 5_9.py

---
+++ Chapter 5/5_9.py
@@ -0,0 +1,9 @@
+users = []
+
+for user in users:
+    if user in users:
+        print(f"Hello {user.title()}, thank you for loggin in again.")
+if "admin" in users:
+    print("\nHello Admin, would you like to see a status report?")
+else:
+    print("We need to find some users!")
