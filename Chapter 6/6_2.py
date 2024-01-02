36f020a816bbf8dd6048216b3146e959ad19429e
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-01-02 16:33:07 +0200

    Create 6_2.py

---
+++ Chapter 6/6_2.py
@@ -0,0 +1,15 @@
+# придумати людей і вилати їм улюблені числа
+
+favorite_numbers = {
+    "alex": 9,
+    "holly": 13,
+    "samuel": 6,
+    "daniel": 3,
+    "marta": 1,
+}
+
+print(f"Alex`s favorite number is {'alex'}.")
+print(f"Holly`s favorite number is {'hilly'}.")
+print(f"Samuel`s favorite number is {'samuel'}.")
+print(f"Daniel`s favorite number is {'daniel'}.")
+print(f"Marta`s favorite number is {'marta'}.")
