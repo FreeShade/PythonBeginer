36420cb2f50d42d726cd81f4b5af2e6743b2ad32
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-01-02 16:33:12 +0200

    Update Dirty_Page.py

--- Dirty Page/Dirty_Page.py
+++ Dirty Page/Dirty_Page.py
@@ -306,12 +306,20 @@
 # del alien_0["points"]
 # print(alien_0)
 
-favorite_languages = {
-    "jen": "python",
-    "sarah": "c",
-    "edvard": "ruby",
-    "phil": "python",
-}
+# favorite_languages = {
+#     "jen": "python",
+#     "sarah": "c",
+#     "edvard": "ruby",
+#     "phil": "python",
+# }
 
-language = favorite_languages["sarah"].title()
-print(f"Sarah`s favorite language is {language}.")
+# language = favorite_languages["sarah"].title()
+# print(f"Sarah`s favorite language is {language}.")
+
+
+alien_0 = {
+    "color": "green",
+    "speed": "slow",
+}
+point_value = alien_0.get("points", "No point value assigned.")
+print(point_value)
