a923e2d1c23361d0e9b6a9c99bf48514ae99b848
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-01-03 13:34:16 +0200

    Update Dirty_Page.py

--- Dirty Page/Dirty_Page.py
+++ Dirty Page/Dirty_Page.py
@@ -379,12 +379,12 @@
 #         print(f"\t{name.title()}, I soo you love {language}!")
 
 
-favorite_languages = {
-    "jen": "python",
-    "sarah": "c",
-    "edward": "ruby",
-    "phil": "python",
-}
+# favorite_languages = {
+#     "jen": "python",
+#     "sarah": "c",
+#     "edward": "ruby",
+#     "phil": "python",
+# }
 
-if "erin" not in favorite_languages:
-    print("Erin, please take our poll!")
+# if "erin" not in favorite_languages:
+#     print("Erin, please take our poll!")
