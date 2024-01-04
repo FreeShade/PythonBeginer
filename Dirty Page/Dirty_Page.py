931b1613b60c354f65250a1393279c9e1cdfd686
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-01-04 12:44:42 +0200

    Update Dirty_Page.py

--- Dirty Page/Dirty_Page.py
+++ Dirty Page/Dirty_Page.py
@@ -430,3 +430,12 @@
 
 
 # Вкладання в пітон(і)
+
+alien_0 = {"color": "green", "points": 5}
+alien_1 = {"color": "yelow", "points": 10}
+alien_2 = {"color": "red", "points": 15}
+
+aliens = [alien_0, alien_1, alien_2]
+
+for alien in aliens:
+    print(alien)
