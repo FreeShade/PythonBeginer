3ba5f2b9abf063404382cf52cd7d3f206c1f6454
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-01-05 13:26:04 +0200

    Update 6_7.py

--- Chapter 6/6_7.py
+++ Chapter 6/6_7.py
@@ -1,11 +1,28 @@
 people = {
-    "first_name": "alex",
-    "last_name": "liberty",
-    "age": 33,
-    "city": "night_city",
+    "alex": {
+        "first_name": "alex",
+        "last_name": "liberty",
+        "age": "33",
+        "city": "night_city",
+    },
+    "mira": {
+        "first_name": "miroslava",
+        "last_name": "liberty",
+        "age": "30",
+        "city": "night_city",
+    },
+    "dobby": {
+        "first_name": "dobby",
+        "last_name": "liberty",
+        "age": "405",
+        "city": "free",
+    },
 }
 
-# print(human["first_name"].title())
-# print(human["last_name"].title())
-# print(human["age"])
-# print(human["city"].title())
+for username, user_info in people.items():
+    print(f"\nUsername : {username.title()}")
+    full_name = f"{user_info['first_name']} {user_info['last_name']}"
+    location = user_info["city"]
+
+    print(f"\tFull name: {full_name.title()}")
+    print(f"\tLocation: {location.title()}")
