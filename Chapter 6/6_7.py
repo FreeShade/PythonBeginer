846f56d5930e9eae94f96eb2941e258ede3e6081
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-01-05 12:56:58 +0200

    Create 6_7.py

---
+++ Chapter 6/6_7.py
@@ -0,0 +1,11 @@
+people = {
+    "first_name": "alex",
+    "last_name": "liberty",
+    "age": 33,
+    "city": "night_city",
+}
+
+# print(human["first_name"].title())
+# print(human["last_name"].title())
+# print(human["age"])
+# print(human["city"].title())
