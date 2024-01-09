3ccbaf1f441699a7c06905d029111d84d57b3ebb
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-01-09 11:46:48 +0200

    Update 6_11.py

--- Chapter 6/6_11.py
+++ Chapter 6/6_11.py
@@ -3,17 +3,19 @@
 cities = {
     "kyiv": {
         "country": "ukraine",
-        "population": "",
-        "fact": "",
+        "population": "2 952 301 ",
+        "fact": 'Kiev known to Russians as the "mother of Russian cities".',
     },
     "london": {
         "country": "unaited kingdom",
-        "population": "",
+        "population": "8 799 728",
         "fact": "London was once the capital of 6 countries at the same time.",
     },
     "tokio": {
         "country": "japan",
-        "population": "",
+        "population": "13 185 502",
         "fact": "Tokyo was called Edo for a very long time",
     },
 }
+
+# print(cities)
