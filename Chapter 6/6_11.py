edf6ed93418b60090b9d3f3e6317e4707ce66340
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-01-09 11:44:30 +0200

    Update 6_11.py

---
+++ Chapter 6/6_11.py
@@ -0,0 +1,19 @@
+# створити словник за містами, країну розташування, цікавий факт, і тд.
+
+cities = {
+    "kyiv": {
+        "country": "ukraine",
+        "population": "",
+        "fact": "",
+    },
+    "london": {
+        "country": "unaited kingdom",
+        "population": "",
+        "fact": "London was once the capital of 6 countries at the same time.",
+    },
+    "tokio": {
+        "country": "japan",
+        "population": "",
+        "fact": "Tokyo was called Edo for a very long time",
+    },
+}
