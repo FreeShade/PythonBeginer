5de29983b8bb1bd94e4afeec44acf9dd95a4421e
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-01-10 11:42:17 +0200

    Update Dirty_Page.py

--- Dirty Page/Dirty_Page.py
+++ Dirty Page/Dirty_Page.py
@@ -533,5 +533,9 @@
 
 # input() функція ставить програму на пауху і чекає , доки користувач введе якийсь текст. Приклад:
 
-message = input("Tell me something, and I will repeat it back to you: ")
-print(message)
+# message = input("Tell me something, and I will repeat it back to you: ")
+# print(message)
+
+
+name = input("Please enter your name: ")
+print(f"\nHello, {name}!")
