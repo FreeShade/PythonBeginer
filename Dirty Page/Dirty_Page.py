61ab91dfb21e881b1fb2f7b40bb92b36024e49bd
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-01-10 11:45:31 +0200

    Update Dirty_Page.py

--- Dirty Page/Dirty_Page.py
+++ Dirty Page/Dirty_Page.py
@@ -541,9 +541,9 @@
 # print(f"\nHello, {name}!")
 
 
-promt = "If you tell us who are, we can personalize the messages you see."
-promt += "\nWhat is your first name ? "
+# promt = "If you tell us who are, we can personalize the messages you see."
+# promt += "\nWhat is your first name ? "
 
-name = input(promt)
+# name = input(promt)
 
-print(f"\nHello, {name} !")
+# print(f"\nHello, {name} !")
