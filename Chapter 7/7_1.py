b039f5289731d6f72b3c871991ac90a9d6c93aae
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-01-11 09:36:39 +0200

    Create 7_1.py

---
+++ Chapter 7/7_1.py
@@ -0,0 +1,4 @@
+# Програма просто питає яку магину знайти і виводить відповідь що шукає її.
+
+car = input("What car do you wanna find ? : ")
+print(f"Let me see if I can find you a {car.title()}.")
