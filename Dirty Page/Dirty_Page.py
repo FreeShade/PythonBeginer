f47260ee929b98b8d2a09ecd32b85211f59845f9
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-01-12 15:36:09 +0200

    Update Dirty_Page.py

--- Dirty Page/Dirty_Page.py
+++ Dirty Page/Dirty_Page.py
@@ -663,3 +663,20 @@
 
 
 # Цикл while і словники та списки
+# # Почати з користувачів, яких треба перевірити,
+# # та порожнього списку підтверджених користувачів.
+# unconfirmed_users = ["alice", "brian", "candace"]
+# confirmed_users = []
+
+# # Перевіряти кожного користувача, доки непідтверджені користувачі не закінчаться.
+# # Переносити кожного перевіреного користувача до списку підтверджених користувачів.
+# while unconfirmed_users:
+#     current_user = unconfirmed_users.pop()
+
+#     print(f"Verifying user : {current_user.title()}")
+#     confirmed_users.append(current_user)
+
+# # Показати всіх підтверджених користувачів
+# print("\nThe following users have been confirmed: ")
+# for confirmed_user in confirmed_users:
+#     print(confirmed_user.title())
