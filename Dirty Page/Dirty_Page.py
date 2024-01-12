41b993eeac128c23e8c0f209611090b58e0f5103
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-01-12 15:41:06 +0200

    Update Dirty_Page.py

--- Dirty Page/Dirty_Page.py
+++ Dirty Page/Dirty_Page.py
@@ -680,3 +680,14 @@
 # print("\nThe following users have been confirmed: ")
 # for confirmed_user in confirmed_users:
 #     print(confirmed_user.title())
+
+
+# remove зі списку, циклом while
+
+pets = ["dog", "cat", "goldfish", "cat", "rebbit", "cat"]
+print(pets)
+
+while "cat" in pets:
+    pets.remove("cat")
+
+print(pets)
