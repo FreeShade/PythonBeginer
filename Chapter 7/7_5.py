3a708241f1ec57ec79d163c77adc8f620253f32e
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-01-12 11:06:55 +0200

    Update 7_5.py

--- Chapter 7/7_5.py
+++ Chapter 7/7_5.py
@@ -5,12 +5,17 @@ customer_age = input("Please tell your age: ")
 customer_age = int(customer_age)
 
 # Напишемо if-else для квитків і цін
-
-if customer_age < 3:
-    tiket_price = 0
-elif customer_age >= 3 and customer_age < 12:
-    tiket_price = 10
-elif customer_age >= 12:
-    tiket_price = 15
-
-print(f"Your tiket cost will be {tiket_price} $.")
+# огорнемо це все циклом while
+while True:
+    if customer_age < 3:
+        tiket_price = 0
+        print(f"Your tiket cost will be {tiket_price} $.")
+        break
+    elif customer_age >= 3 and customer_age < 12:
+        tiket_price = 10
+        print(f"Your tiket cost will be {tiket_price} $.")
+        break
+    elif customer_age >= 12:
+        tiket_price = 15
+        print(f"Your tiket cost will be {tiket_price} $.")
+        break
