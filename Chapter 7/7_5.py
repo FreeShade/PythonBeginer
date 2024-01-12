db8f45d710edc4206bfb44b7fb4e64aae065d3c4
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-01-12 11:41:07 +0200

    Update 7_5.py

--- Chapter 7/7_5.py
+++ Chapter 7/7_5.py
@@ -1,21 +1,27 @@
 # Є різні категорії квитків в кіно, залежно від віку
 # Питаємо вік
-
-customer_age = input("Please tell your age: ")
-customer_age = int(customer_age)
+active_status = True
 
 # Напишемо if-else для квитків і цін
 # огорнемо це все циклом while
-while True:
-    if customer_age < 3:
-        tiket_price = 0
-        print(f"Your tiket cost will be {tiket_price} $.")
-        break
-    elif customer_age >= 3 and customer_age < 12:
-        tiket_price = 10
-        print(f"Your tiket cost will be {tiket_price} $.")
-        break
-    elif customer_age >= 12:
-        tiket_price = 15
-        print(f"Your tiket cost will be {tiket_price} $.")
-        break
+while active_status == True:
+    customer_age = input("Please tell your age: ")
+
+    if customer_age == "quit":
+        active_status = False
+
+    else:
+        customer_age = int(customer_age)
+
+        if customer_age < 3:
+            tiket_price = 0
+            print(f"Your tiket cost will be {tiket_price} $.")
+
+        elif customer_age >= 3 and customer_age < 12:
+            tiket_price = 10
+            print(f"Your tiket cost will be {tiket_price} $.")
+
+        elif customer_age >= 12:
+            tiket_price = 15
+            print(f"Your tiket cost will be {tiket_price} $.")
+# тепер працює як я хотів
