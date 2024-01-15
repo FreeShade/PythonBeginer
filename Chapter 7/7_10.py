e6c8f849e8625c50f95661eff567d4ac1ee8e9e7
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-01-15 16:27:27 +0200

    Update 7_10.py

--- Chapter 7/7_10.py
+++ Chapter 7/7_10.py
@@ -17,3 +17,21 @@
 # print("\n ---- Poll Results ----")
 # for user, user_response in user_responses.items():
 #     print(f"{user.title()} would like to travel to {user_response.title()}.")
+
+
+# Функції , все стає складніше , я вже бувало продовбувався з ними, дуже складно, але ефективно
+
+
+# def greet_user():
+#     """Показати просте вітання."""
+#     print("Hello!")
+
+# greet_user()
+
+
+def greet_user(user_name):
+    """Показати просте вітання."""
+    print(f"Hello, {user_name.title()}!")
+
+
+greet_user(input("Something: "))
