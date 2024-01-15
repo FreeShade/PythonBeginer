453872847defed9c1a38692b9e8a0c3985aeeddf
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-01-15 16:32:56 +0200

    Update 8_1.py

---
+++ Chapter 8/8_1.py
@@ -0,0 +1,10 @@
+# Напишіть функцію яка виводить якесь повідомлення
+
+
+def display_message():
+    """Another message about another step to GLORY !"""
+    print("SOme dificult message, from dificult function")
+    print("Why its def for FUNction , better use Func or something...")
+
+
+display_message()
