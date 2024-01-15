3de8a02c2e3877dcba655750696d37c83af4bf78
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-01-15 09:35:15 +0200

    Create 7_10.py

---
+++ Chapter 7/7_10.py
@@ -0,0 +1,19 @@
+# Напишіть програму, що питатиму користувача про подорож їхньої мрії.
+
+user_responses = {}
+polling_active = True
+while polling_active:
+    user = input("\nWhat is your name? ")
+    user_response = input("Which place would you like to travel someday ? ")
+    # Зберегти відповідь у словник
+    user_responses[user] = user_response
+
+    # Дізнатись чи ще хтось буде проходити опитування.
+    repeat = input("Would you like to let another persone respond? (yes/no) ")
+
+    if repeat == "no":
+        polling_active = False
+
+print("\n ---- Poll Results ----")
+for user, user_response in user_responses.items():
+    print(f"{user.title()} would like to travel to {user_response.title()}.")
