8e4f682a37d2c600f0977228b48cc34702477df2
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-01-18 14:15:55 +0200

    Update Dirty_Page.py

--- Dirty Page/Dirty_Page.py
+++ Dirty Page/Dirty_Page.py
@@ -788,12 +788,30 @@
 # print(musician)
 
 
-# Розширимо функціонал функції вище
-def get_formatted_name(first_name, middle_name, last_name):
+# # Розширимо функціонал функції вище
+# def get_formatted_name(first_name, middle_name, last_name):
+#     """Повернути відформатоване повне ім'я."""
+#     full_name = f"{first_name} {middle_name} {last_name}"
+#     return full_name.title()
+
+
+# musician = get_formatted_name("john", "lee", "hooker")
+# print(musician)
+
+# Змінимо функцію щоб друге ім'я було не обов'язковим
+
+
+def get_formatted_name(first_name, last_name, middle_name=""):
     """Повернути відформатоване повне ім'я."""
-    full_name = f"{first_name} {middle_name} {last_name}"
+    if middle_name:
+        full_name = f"{first_name} {middle_name} {last_name}"
+    else:
+        full_name = f"{first_name} {last_name}"
     return full_name.title()
 
 
-musician = get_formatted_name("john", "lee", "hooker")
+musician = get_formatted_name("jimi", "hendrix")
+print(musician)
+
+musician = get_formatted_name("john", "hooker", "lee")
 print(musician)
