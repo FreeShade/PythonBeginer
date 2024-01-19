11c12ea1c52ea229b6756b2cb33ed3b75aeea0c6
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-01-19 12:46:18 +0200

    Update Dirty_Page.py

--- Dirty Page/Dirty_Page.py
+++ Dirty Page/Dirty_Page.py
@@ -820,11 +820,23 @@
 # Повернення словника
 
 
-def build_person(first_name, last_name):
+# def build_person(first_name, last_name):
+#     """Повернути словник з інформацією про людину."""
+#     person = {"first": first_name, "last": last_name}
+#     return person
+
+
+# musician = build_person("jimi", "hendrix")
+# print(musician)
+
+
+def build_person(first_name, last_name, age=None):
     """Повернути словник з інформацією про людину."""
     person = {"first": first_name, "last": last_name}
+    if age:
+        person["age"] = age
     return person
 
 
-musician = build_person("jimi", "hendrix")
+musician = build_person("jimi", "hendrix", age=27)
 print(musician)
