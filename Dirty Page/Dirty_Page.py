517da19e98c6b198d04fa92f86b32285b8473971
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-01-19 13:10:36 +0200

    Update Dirty_Page.py

--- Dirty Page/Dirty_Page.py
+++ Dirty Page/Dirty_Page.py
@@ -842,17 +842,39 @@
 # print(musician)
 
 
-def get_formatted_name(first_name, last_name, age=None):
-    """Повернути словник з інформацією про людину."""
+# def get_formatted_name(first_name, last_name, age=None):
+#     """Повернути словник з інформацією про людину."""
+#     full_name = f"{first_name} {last_name}"
+#     return full_name.title()
+
+
+# # Це нескінченний циклm
+# while True:
+#     print("\nPlease tell me your name: ")
+#     f_name = input("First name: ")
+#     l_name = input("Last name: ")
+
+#     formatted_name = get_formatted_name(f_name, l_name)
+#     print(f"\nHello, {formatted_name} !")
+
+
+def get_formatted_name(first_name, last_name):
+    """Повернути відформатоване повне ім'я"""
     full_name = f"{first_name} {last_name}"
     return full_name.title()
 
 
-# Це нескінченний циклm
 while True:
     print("\nPlease tell me your name: ")
+    print("(enter 'q' at any time to quit)")
+
     f_name = input("First name: ")
+    if f_name == "q":
+        break
+
     l_name = input("Last name: ")
+    if l_name == "q":
+        break
 
     formatted_name = get_formatted_name(f_name, l_name)
-    print(f"\nHello, {formatted_name} !")
+    print(f"\nHello, {formatted_name}!")
