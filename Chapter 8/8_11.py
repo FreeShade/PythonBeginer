b3c8232b12190b855385b86794fa032164f0d738
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-01-22 15:17:15 +0200

    Update 8_11.py

---
+++ Chapter 8/8_11.py
@@ -0,0 +1,29 @@
+# Почніть на базі задачі 8_10.ру . Викличте функцію сенд меседж, передавши її КОПІЮ до списку
+# повідомлень , оригінальний перелік повинен зберегтись.
+
+
+def send_message(messages_list, sent_messages):
+    """Вище прописав що воно робить , незграбно , але самостійно."""
+    while messages_list:
+        current_message = messages_list.pop()
+        print(f"Printing message: {current_message}.")
+        sent_messages.append(current_message)
+
+
+def show_messages(some_messages_list):
+    """Виводить кожне повідомлення зі списку"""
+    for some_message in some_messages_list:
+        print(some_message)
+
+
+messages_list = ["This is a message.", "this is a message too.", "Same thing."]
+sent_messages = []
+
+
+send_message(messages_list[:], sent_messages)
+show_messages(sent_messages)
+show_messages(messages_list)
+
+# вкінці виведіть обидва переліки. Які збереглися.
+print(messages_list)
+print(sent_messages)
