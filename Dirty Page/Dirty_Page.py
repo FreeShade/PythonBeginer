1891e63d0cdb15618c8314c498f1f7dab62e43db
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-01-22 14:38:24 +0200

    Update Dirty_Page.py

--- Dirty Page/Dirty_Page.py
+++ Dirty Page/Dirty_Page.py
@@ -916,26 +916,30 @@
 # Реорганізація коду вище
 
 
-def print_models(unprinted_designs, completed_models):
-    """
-    Симулювати друк кожного креслення, доки всі не закінчаться.
-    Перенести кожен малюнок до completed_models після друку.
-    """
-    while unprinted_designs:
-        current_design = unprinted_designs.pop()
-        print(f"Printing model: {current_design}")
-        completed_models.append(current_design)
+# def print_models(unprinted_designs, completed_models):
+#     """
+#     Симулювати друк кожного креслення, доки всі не закінчаться.
+#     Перенести кожен малюнок до completed_models після друку.
+#     """
+#     while unprinted_designs:
+#         current_design = unprinted_designs.pop()
+#         print(f"Printing model: {current_design}")
+#         completed_models.append(current_design)
 
 
-def show_completed_models(completed_models):
-    """Показати всі надруковані моделі."""
-    print("\nThe following models have been printed.")
-    for completed_model in completed_models:
-        print(completed_model)
+# def show_completed_models(completed_models):
+#     """Показати всі надруковані моделі."""
+#     print("\nThe following models have been printed.")
+#     for completed_model in completed_models:
+#         print(completed_model)
 
 
-unprinted_designs = ["phone case", "robot pendant", "dodecahedron"]
-completed_models = []
+# unprinted_designs = ["phone case", "robot pendant", "dodecahedron"]
+# completed_models = []
 
-print_models(unprinted_designs, completed_models)
-show_completed_models(completed_models)
+# print_models(unprinted_designs, completed_models)
+# show_completed_models(completed_models)
+
+
+# Як не дозволити функції видозмінити список
+# як , як , копіювати замість зміни, список в функції заміни зміну на копію і бек ап залишиться
