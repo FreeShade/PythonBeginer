cf02a89203d32c4512092aebdeeef6933bb23342
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-01-23 15:22:28 +0200

    Update Dirty_Page.py

--- Dirty Page/Dirty_Page.py
+++ Dirty Page/Dirty_Page.py
@@ -956,12 +956,25 @@
 # make_pizza("mushroom", "green pepers", "extra cheese")
 
 
-def make_pizza(*toppings):
-    """Описати піцу, яку ми збираємося приготувати."""
-    print("\nMaking a pizza with the following toppings: ")
+# def make_pizza(*toppings):
+#     """Описати піцу, яку ми збираємося приготувати."""
+#     print("\nMaking a pizza with the following toppings: ")
+#     for topping in toppings:
+#         print(f" - {topping}")
+
+
+# make_pizza("pepperoni")
+# make_pizza("mushrooms", "green peppers", "extra cheese")
+
+# додамо параметр розміру піци
+
+
+def make_pizza(size, *toppings):
+    """Описати піцу,яку ми збираємося приготувати."""
+    print(f"\nMaking a {size}-inch pizza with the following toppings:")
     for topping in toppings:
         print(f" - {topping}")
 
 
-make_pizza("pepperoni")
-make_pizza("mushrooms", "green peppers", "extra cheese")
+make_pizza(16, "pepperoni")
+make_pizza(12, "mushrooms", "green peppers", "extra cheese")
