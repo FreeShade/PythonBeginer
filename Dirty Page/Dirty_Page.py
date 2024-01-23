cdc5e5d071663bdeb8e002857a2c45dadb6e1697
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-01-23 15:38:01 +0200

    Update Dirty_Page.py

--- Dirty Page/Dirty_Page.py
+++ Dirty Page/Dirty_Page.py
@@ -966,15 +966,37 @@
 # make_pizza("pepperoni")
 # make_pizza("mushrooms", "green peppers", "extra cheese")
 
-# додамо параметр розміру піци
+# # додамо параметр розміру піци
 
 
-def make_pizza(size, *toppings):
-    """Описати піцу,яку ми збираємося приготувати."""
-    print(f"\nMaking a {size}-inch pizza with the following toppings:")
-    for topping in toppings:
-        print(f" - {topping}")
+# def make_pizza(size, *toppings):
+#     """Описати піцу,яку ми збираємося приготувати."""
+#     print(f"\nMaking a {size}-inch pizza with the following toppings:")
+#     for topping in toppings:
+#         print(f" - {topping}")
 
 
-make_pizza(16, "pepperoni")
-make_pizza(12, "mushrooms", "green peppers", "extra cheese")
+# make_pizza(16, "pepperoni")
+# make_pizza(12, "mushrooms", "green peppers", "extra cheese")
+
+
+# #*args - параметр збору довільних позиційних аргументів
+
+
+# Довільні ключові аргументи **kwargs
+
+
+def build_profile(first, last, **user_info):
+    """Створити словник, що міститиме всю інформацію про користувача."""
+    user_info["first_name"] = first
+    user_info["last_name"] = last
+    return user_info
+
+
+user_profile = build_profile(
+    "albert", "einstein", location="princeton", field="physics"
+)
+
+print(user_profile)
+
+# **kwargs - набір неописаних ключових аргументів.
