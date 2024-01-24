417b4b4ca9f8a3bf08c7cdd56bd7280d349d5e8b
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-01-24 14:16:15 +0200

    Create making_pizzas.py

---
+++ Dirty Page/making_pizzas.py
@@ -0,0 +1,11 @@
+from pizza import make_pizza
+
+# import pizza
+# #обидва варіанти валідні
+# pizza.make_pizza(16, "pepperoni")
+# pizza.make_pizza(12, "mushrooms", "green peppers", "extra cheese")
+
+
+make_pizza(16, "pepperoni")
+make_pizza(12, "mushrooms", "green peppers", "extra cheese")
+# module_name.function_name()
