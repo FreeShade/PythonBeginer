48894825cb060a7d8197bcd99f079c3a8f237615
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-01-24 14:16:18 +0200

    Create pizza.py

---
+++ Dirty Page/pizza.py
@@ -0,0 +1,8 @@
+def make_pizza(size, *toppings):
+    """Описати піццу яку ми збираємося готувати."""
+    print(f"\nMaking a {size}-inch pizza with the following toppings: ")
+    for topping in toppings:
+        print(f" - {topping}")
+
+
+# module_name.function_name()
