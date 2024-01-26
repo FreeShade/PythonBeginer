4b1c6728c1bac19fcd5cd8977a0f65db97f5cc81
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-01-26 10:03:24 +0200

    Update Dirty_Page.py

--- Dirty Page/Dirty_Page.py
+++ Dirty Page/Dirty_Page.py
@@ -1034,3 +1034,9 @@ class Dog:
     def roll_over(self):
         """Cимулювати команду 'лежати'."""
         print(f"{self.name} rolled over!")
+
+
+my_dog = Dog("Willie", 6)
+
+print(f"My dog`s name is {my_dog.name}.")
+print(f"My dog is {my_dog.age} years age.")
