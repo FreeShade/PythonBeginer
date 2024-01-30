a0960e14a76eacc53c0df91a5fc39600f41e486a
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-01-30 15:19:56 +0200

    Update Dirty_Page.py

--- Dirty Page/Dirty_Page.py
+++ Dirty Page/Dirty_Page.py
@@ -1090,8 +1090,18 @@ class Car:
         self.make = make
         self.model = model
         self.year = year
+        self.odometer_reading = 0  # не обов'язково передавати в ІНІТ .
 
     def get_describptive_name(self):
         """Повернути відформатоване змістовне ім'я."""
         long_name = f"{self.year} {self.make} {self.model}"
         return long_name.title()
+
+    def read_odometer(self):
+        """Вивести повідомлення з пробігом машини"""
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+
+my_new_car = Car("audi", "a4", 2019)
+print(my_new_car.get_describptive_name())
+my_new_car.read_odometer()
