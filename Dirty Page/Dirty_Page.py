59e47c648b4a3686acfacacfcecfb3461a4aacbb
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-02-13 09:37:11 +0200

    Update Dirty_Page.py

--- Dirty Page/Dirty_Page.py
+++ Dirty Page/Dirty_Page.py
@@ -1309,32 +1309,45 @@
 
 
 # створить модуль що складається тільки з класу Car
-class Car:
-    """Знову модулюємо автівку."""
+# class Car:
+#     """Знову модулюємо автівку."""
 
-    def __init__(self, make, model, year):
-        self.make = make
-        self.model = model
-        self.year = year
-        self.odometer_reading = 0
+#     def __init__(self, make, model, year):
+#         self.make = make
+#         self.model = model
+#         self.year = year
+#         self.odometer_reading = 0
 
-    def get_descriptive_name(self):
-        long_name = f"{self.year} {self.make} {self.model}"
-        return long_name.title()
+#     def get_descriptive_name(self):
+#         long_name = f"{self.year} {self.make} {self.model}"
+#         return long_name.title()
 
-    def read_odometer(self):
-        print(f"This car has {self.odometer_reading} miles on it.")
+#     def read_odometer(self):
+#         print(f"This car has {self.odometer_reading} miles on it.")
 
-    def update_odometer(self, mileage):
-        if mileage >= self.odometer_reading:
-            self.odometer_reading = mileage
-        else:
-            print("You can`t roll back an odometer!")
+#     def update_odometer(self, mileage):
+#         if mileage >= self.odometer_reading:
+#             self.odometer_reading = mileage
+#         else:
+#             print("You can`t roll back an odometer!")
 
-    def increment_odometer(self, miles):
-        self.odometer_reading += miles
+#     def increment_odometer(self, miles):
+#         self.odometer_reading += miles
 
 
 # Збереження кількох класів в одному модулі (car.py)
 # Імпортування багатьлх класів з одного модуля. (my_cars.py)
-#
+# Імпорт цілого модуля.
+
+# Імпортування всіх класів з модуля
+# from module_name import*
+
+
+# Імпортування модуля в інший модуль
+# (my_car.py) все там.
+
+
+# Використання псевдонімів
+from electric_car import ElectricCar as EC
+
+my_tesla = EC('tesla', 'roadster', 2019)
