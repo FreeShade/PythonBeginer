1b83d22752e216de03f79d722abcc71aae50762c
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-02-14 17:09:19 +0200

    Create restaraunt_module.py

---
+++ Chapter 9/restaraunt_module.py
@@ -0,0 +1,16 @@
+class Restaraunt:
+    """Моделюємо ресторан"""
+
+    def __init__(self, restaraunt_name, cuisine_type):
+        """Ініцібвати атрибути назва та тип кухні."""
+        self.restaraunt_name = restaraunt_name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaraunt(self):
+        """Описує ресторан"""
+        print(f"A Restaraunt name is {self.restaraunt_name}.")
+        print(f"THe restaraunt cuisine is {self.cuisine_type}.")
+
+    def open_restaraunt(self):
+        """Повідомляє що заклад працює"""
+        print(f"{self.restaraunt_name.title()} is open now.")
