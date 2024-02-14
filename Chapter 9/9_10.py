87149b51a00d6d7671cac7c4500674c7d5948ada
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-02-14 17:08:28 +0200

    Update 9_10.py

---
+++ Chapter 9/9_10.py
@@ -0,0 +1,10 @@
+from restaraunt_module import Restaraunt
+
+# Екземпляр класу
+rest = Restaraunt("mc`donalds", "american")
+# Виклик методів
+rest.describe_restaraunt()
+rest.open_restaraunt()
+
+
+# Показати що імпорт працює.
