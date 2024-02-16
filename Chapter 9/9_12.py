922c995c089a123132b03f18bd3a69fa52402330
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-02-16 08:05:45 +0200

    Update 9_12.py

--- Chapter 9/9_12.py
+++ Chapter 9/9_12.py
@@ -3,3 +3,11 @@ from admin import Admin, Privileges
 
 admin_user = Privileges("axel", "suter", 77, "haven", "he")
 admin_user.show_privileges()
+
+
+# import admin
+
+
+# admin_user = admin.Privileges("axel", "suter", 77, "haven", "he")
+# admin_user.show_privileges()
+# Few wariations
