909bbe1b0ccca737eb62a169f67d028c8c826765
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-02-21 15:50:28 +0200

    Update file_reader.py

--- Dirty Page/file_reader.py
+++ Dirty Page/file_reader.py
@@ -1,3 +1,10 @@
+import os
+
+
+os.chdir(r"C:\Users\Natrix\Desktop\repositories\PythonBeginer\Dirty Page")
+
+print("Поточний каталог:", os.getcwd())
+
 with open("pi_d.txt") as file_object:
     contents = file_object.read()
 print(contents)
