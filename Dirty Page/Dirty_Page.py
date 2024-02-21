7b0d1198a66a46816b484155f33a2f2bb74852e1
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-02-21 16:06:40 +0200

    Update Dirty_Page.py

--- Dirty Page/Dirty_Page.py
+++ Dirty Page/Dirty_Page.py
@@ -1371,3 +1371,19 @@
 
 
 # Робота з файлами та вийнятки.
+
+with open("Dirty Page/pi_d.txt") as file_object:
+    contents = file_object.read()
+print(contents.rstrip())
+
+
+# import os
+
+
+# os.chdir(r"C:\Users\Natrix\Desktop\repositories\PythonBeginer\Dirty Page")
+# print("Поточний каталог:", os.getcwd())
+# буква "r" в коді нижче допомагає не дублювати "\\" дуже зручно
+file_path = r"C:\Users\Natrix\Desktop\repositories\PythonBeginer\Dirty Page\pi_d.txt"
+with open(file_path) as file_object:
+    contents = file_object.read()
+print(contents)
