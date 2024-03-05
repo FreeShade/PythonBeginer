e43070b768920bf70dbdf00f7551bf9c0d17923e
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-03-05 15:29:11 +0200

    Update Dirty_Page.py

--- Dirty Page/Dirty_Page.py
+++ Dirty Page/Dirty_Page.py
@@ -1457,8 +1457,8 @@
 
 # Допис у файл
 
-filename = "programing.txt"
+# filename = "programing.txt"
 
-with open(filename, "a") as file:
-    file.write("I also love finding meaning in large dataset. \n")
-    file.write("I love creating apps that can run in browser. \n")
+# with open(filename, "a") as file:
+#     file.write("I also love finding meaning in large dataset. \n")
+#     file.write("I love creating apps that can run in browser. \n")
