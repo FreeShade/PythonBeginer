f9f0f89e9b3055a77b825b73338ba4eaf7cce1fe
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-04-21 14:42:19 +0300

    Update remember_me.py

--- Dirty Page/remember_me.py
+++ Dirty Page/remember_me.py
@@ -1,30 +1,40 @@
+# # import json
+
+# # username = input('What is your name ? ')
+
+# # filename = 'username.json'
+# # with open(filename, 'w') as f:
+# #     json.dump(username, f)
+# #     print(f"We'll remembr you when you com back, {username}!")
+
+
 # import json
 
-# username = input('What is your name ? ')
 
-# filename = 'username.json'
-# with open(filename, 'w') as f:
-#     json.dump(username, f)
-#     print(f"We'll remembr you when you com back, {username}!")
-
-
-import json
-
-
-# Рефакторинг
-def greet_user():
-    """Привітати користувача на ім'я"""
-    filename = "username.json"
-    try:
-        with open(filename) as f:
-            username = json.load(f)
-    except FileNotFoundError:
-        username = input("What is your name? ")
-        with open(filename, "w") as f:
-            json.dump(username, f)
-            print(f"We`ll remember you when you come back, {username}!")
-    else:
-        print(f"Welcome back, {username}!")
+# def get_stored_username():
+#     """Видобути збережене ім'я, якщо таке є."""
+#     filename = "username.json"
+#     try:
+#         with open(filename) as f:
+#             username = json.load(f)
+#     except FileNotFoundError:
+#         return None
+#     else:
+#         return username
 
 
-greet_user()
+# # Рефакторинг
+# def greet_user():
+#     """Привітати користувача на ім'я"""
+#     username = get_stored_username()
+#     if username:
+#         print(f"Welcome back, {username}!")
+#     else:
+#         username = input("What is your name? ")
+#         filename = "username.json"
+#         with open(filename, "w") as f:
+#             json.dump(username, f)
+#             print(f"We'll remembr you when you com back, {username}!")
+
+
+# greet_user()
