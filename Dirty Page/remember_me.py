a17b890e8d67c8d4a42455d4599d23bc6b3a69f5
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-04-21 14:13:30 +0300

    Update remember_me.py

--- Dirty Page/remember_me.py
+++ Dirty Page/remember_me.py
@@ -1,8 +1,30 @@
+# import json
+
+# username = input('What is your name ? ')
+
+# filename = 'username.json'
+# with open(filename, 'w') as f:
+#     json.dump(username, f)
+#     print(f"We'll remembr you when you com back, {username}!")
+
+
 import json
 
-username = input('What is your name ? ')
 
-filename = 'username.json'
-with open(filename, 'w') as f:
-    json.dump(username, f)
-    print(f"We'll remembr you when you com back, {username}!")
+# Рефакторинг
+def greet_user():
+    """Привітати користувача на ім'я"""
+    filename = "username.json"
+    try:
+        with open(filename) as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        username = input("What is your name? ")
+        with open(filename, "w") as f:
+            json.dump(username, f)
+            print(f"We`ll remember you when you come back, {username}!")
+    else:
+        print(f"Welcome back, {username}!")
+
+
+greet_user()
