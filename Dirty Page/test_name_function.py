8f312694f0bcfee6ad477bbcb7c7c06036a62aa4
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-04-26 14:24:56 +0300

    Update test_name_function.py

--- Dirty Page/test_name_function.py
+++ Dirty Page/test_name_function.py
@@ -10,6 +10,11 @@ class NamesTestCase(unittest.TestCase):
         formated_name = get_formated_name("janis", "joplin")
         self.assertEqual(formated_name, "Janis Joplin")
 
+    def test_first_last_middle_name(self):
+        """Чи працює з іменами на кшталт 'Wolfgang Amadeus Mozart'?"""
+        formated_name = get_formated_name("wolfgang", "mozart", "amadeus")
+        self.assertEqual(formated_name, "Wolfgang Amadeus Mozart")
+
 
 if __name__ == "__main__":
     unittest.main()
