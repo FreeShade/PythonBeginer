a4650124ba7f4a4e833c8f6eee9daf11b78a5d68
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-05-01 20:37:01 +0300

    Update Dirty Page

---
+++ Dirty Page/test_survey.py
@@ -0,0 +1,30 @@
+import unittest
+from survey import AnonymousSurvey
+
+class TestAnonymousSurvey(unittest.TestCase):
+    '''Тестм для класу AnonymousSurvey'''
+    
+    def test_store_single_response(self):
+        '''Перевірити, чи одиночна відповідь зберігається правильно.'''
+        question = "What language did you first learn to speak?"
+        my_survey = AnonymousSurvey(question)
+        my_survey.store_response('English')
+        self.assertIn('English', my_survey.responses)
+        
+    def test_store_three_responses(self):
+        """
+        Перевірити, чи три індивідуальних
+        відповіді зберігаються правильно.
+        """
+        
+        question = 'What language did you first learn to speak'
+        my_survey = AnonymousSurvey(question)
+        responses = ['English', 'Spanish', 'Ukrainian', 'Mandarin']
+        for response in responses:
+            my_survey.store_response(response)
+            
+        for response in responses:
+            self.assertIn(response, my_survey.responses)
+        
+if __name__ == '__main__':
+    unittest.main()
