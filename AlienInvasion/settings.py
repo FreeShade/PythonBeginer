6c2ef8eec2cad0a77e0e2b4c042b5c02dc1a096d
FreeShade <84828176+FreeShade@users.noreply.github.com>
2024-07-27 17:12:52 +0300

    Create settings.py

---
+++ AlienInvasion/settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """Клас для збереження всіх налаштувань"""
+    
+    def __init__(self):
+        """ігіціалізація налаштувань гри"""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (34, 139, 34)
